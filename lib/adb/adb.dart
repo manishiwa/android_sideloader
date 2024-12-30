01bbdf127b430443951e2dd224bff78d36240b92
Ryan <rwalters@seven.com>
2024-12-30 14:18:39 -0600

    feat: add kill-server and start-server

--- lib/adb/adb.dart
+++ lib/adb/adb.dart
@@ -15,15 +15,43 @@ class Adb {
   }) async {
     try {
       final result = await (await _shell).run(
-          '${await AdbPath.adbPath} '
-          '${device != null ? "-s $device " : ""}'
-          'install $filePath'
+        '${await AdbPath.adbPath} '
+        '${device != null ? "-s $device " : ""}'
+        'install $filePath'
       );
-      Log.i(result.outText);
+      Log.i("Successfully installed APK:\n${result.outText}");
       onSuccess(result.outText);
     } catch (e) {
       Log.e("Error installing APK", error: e);
       onFailure("Error: $e");
+    }
+  }
+
+  static Future<bool> killServer() async {
+    try {
+      final result = await (await _shell).run(
+        '${await AdbPath.adbPath} '
+        'kill-server'
+      );
+      Log.i("Successfully killed ADB server:\n${result.outText}");
+      return true;
+    } catch (e) {
+      Log.e("Error killing ADB server", error: e);
+      return false;
+    }
+  }
+
+  static Future<bool> startServer() async {
+    try {
+      final result = await (await _shell).run(
+        '${await AdbPath.adbPath} '
+        'start-server'
+      );
+      Log.i("Successfully started ADB server:\n${result.outText}");
+      return true;
+    } catch (e) {
+      Log.e("Error starting ADB server", error: e);
+      return false;
     }
   }
 }
